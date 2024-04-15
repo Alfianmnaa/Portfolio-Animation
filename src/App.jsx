3002c547cc2450b74872459b91f0559969e9c55c
Alfian Maulana <alfianmna@gmail.com>
2024-04-15 13:31:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Skills</section>
+      <section>Paralax</section>
+      <section>Services</section>
+      <section>Paralax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </>
+  );
+}
+
+export default App;
